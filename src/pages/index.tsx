ea775e67b87032c53f551a87f2c358d87ea60f39
Carlos Eduardo <carloseduardo@cenarioweb.com.br>
2021-04-14 00:19:46 -0300

    Aplicando estilos globais

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,12 @@
 import Head from 'next/head';
 
-import styles from '../styles/home.module.scss';
-
 export default function Home() {
   return (
     <>
       <Head>
         <title>Início | ig.news</title>
       </Head>
-      <h1 className={styles.title}>ig<span>news</span></h1>
+      <h1>ig.news</h1>
     </>
   )
 }
