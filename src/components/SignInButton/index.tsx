ac80be1eb1ca871bfe094a5e24e3bbeebd077b02
Carlos Eduardo <carloseduardo@cenarioweb.com.br>
2021-04-15 22:49:39 -0300

    Criando o botão de login com o GitHub

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,27 @@
+import { FaGithub } from 'react-icons/fa';
+import { FiX } from 'react-icons/fi';
+
+import styles from './styles.module.scss';
+
+export function SignInButton () {
+  const isUserLoggedIn = true;
+
+  return isUserLoggedIn ? (
+    <button
+      type="button"
+      className={styles.signInButton}
+    >
+      <FaGithub className={styles.loggedInIcon} />
+      Carlos Eduardo
+      <FiX className={styles.signOutIcon} />
+    </button>
+  ) : (
+    <button
+      type="button"
+      className={styles.signInButton}
+    >
+      <FaGithub className={styles.loggedOutIcon} />
+      Sign in with GitHub
+    </button>
+  )
+}
