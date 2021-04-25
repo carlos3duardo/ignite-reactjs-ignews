3bb4fbea3d94cc217fbcbf62a2277e961857e5f1
Carlos Eduardo <carloseduardo@cenarioweb.com.br>
2021-04-25 16:39:52 -0300

    Aplicando Static Site Generation (SSG) Consultando o preço do produto e criando uma página estática com a informação

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 
 import { stripe } from '../services/stripe';
 import { SubscribeButton } from '../components/SubscribeButton';
@@ -41,7 +41,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve(process.env.STRIPE_PRICE_ID, {
     expand: ['product']
   });
@@ -57,6 +57,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24 // 24 hours
   }
 }
