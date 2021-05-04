efdee2990240d54693342849a494b1be4b5b18d9
Carlos Eduardo <carloseduardo@cenarioweb.com.br>
2021-05-04 17:17:53 -0300

    Ouvindo mais webhooks do Stripe

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { Readable } from 'stream';
 import Stripe from "stripe";
 import { stripe } from "../../services/stripe";
+import { saveSubscription } from "../_lib/manageSubscription";
 
 async function buffer(readable: Readable) {
   const chunks = [];
@@ -22,7 +23,9 @@ export const config = {
 }
 
 const relevantEvents = new Set([
-  'checkout.session.completed'
+  'checkout.session.completed',
+  'customer.subscription.updated',
+  'customer.subscription.deleted'
 ]);
 
 export default async (request: NextApiRequest, response: NextApiResponse) => {
@@ -44,10 +47,44 @@ export default async (request: NextApiRequest, response: NextApiResponse) => {
     }
 
     if (relevantEvents.has(event.type)) {
-      console.log('Evento recebido', event);
+      try {
+        switch (event.type) {
+          case 'customer.subscription.updated':
+          case 'customer.subscription.deleted':
+
+            const subscription = event.data.object as Stripe.Subscription;
+
+            await saveSubscription(
+              subscription.id,
+              subscription.customer.toString(),
+              false
+            );
+
+            break;
+          case 'checkout.session.completed':
+
+            const checkoutSession = event.data.object as Stripe.Checkout.Session;
+
+            await saveSubscription(
+              checkoutSession.subscription.toString(),
+              checkoutSession.customer.toString(),
+              true
+            );
+
+            break;
+          default:
+            throw new Error('Unhanded event.')
+        }
+      } catch (err) {
+        // notificar erro
+        response.json({ message: err.message });
+      }
     }
 
-    response.json({ message: 'Event recieved.' });
+    response.json({
+      message: 'Hello!'
+    });
+
 
   } else {
 
